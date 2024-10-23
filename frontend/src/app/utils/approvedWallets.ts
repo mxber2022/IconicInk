a35ac785765b4b9e697ea5ec3fcfa3313551dd00
mxber2022 <mxber2022@gmail.com>
2024-10-23 12:29:02 +0700

    express deployed

--- frontend/src/app/utils/approvedWallets.ts
+++ frontend/src/app/utils/approvedWallets.ts
@@ -1,7 +1,7 @@
 // utils/fetchWallets.ts
 export const approvedWallets = async (): Promise<string[]> => {
     try {
-        const response = await fetch('http://localhost:4000/api/approved-wallets');
+        const response = await fetch('https://server-mxber2022-mxber2022s-projects.vercel.app/api/approved-wallets');
         
         if (!response.ok) {
             throw new Error(`Failed to fetch wallets, status: ${response.status}`);
